6e9d4c0c2c510851f04a25fb9ab84a90b5c665cc
Mafii <mafi@fog.im>
2016-11-19 00:49:40 +0100

    Add Exception Extension Methods

--- ChainSharp/Core/ExceptionExtensions.cs
+++ ChainSharp/Core/ExceptionExtensions.cs
@@ -1,7 +1,22 @@
-﻿namespace ChainSharp.Core
+﻿using System;
+
+namespace ChainSharp.Core
 {
   public static class ExceptionExtensions
   {
-    
+    public static void Throw<T>(this object @object, string message) where T : Exception
+    {
+      throw (T)Activator.CreateInstance(typeof(T), message);
+    }
+
+    public static void ThrowIfNull(this object @object, string name)
+    {
+      if (@object == null) { throw new ArgumentNullException(name + "is not allowed to be null"); }
+    }
+
+    public static void ThrowIfNull(this object @object)
+    {
+      if (@object == null) { throw new ArgumentNullException(); }
+    }
   }
 }
