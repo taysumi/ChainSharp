270d0ed83a069b3089a9bbfbb8cdaeac58bce98c
Mafii <mafi@fog.im>
2016-11-19 23:08:10 +0100

    Change .sln and project file to VS2017RC

--- ChainSharp/Linq/Extensions.cs
+++ ChainSharp/Linq/Extensions.cs
@@ -5,7 +5,7 @@ namespace ChainSharp.Linq
 {
   public static class Extensions
   {
-    public static void ForEach<T>(IEnumerable<T> enumerable, Action<T> action)
+    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
     {
       foreach (T item in enumerable) { action(item); }
     }
